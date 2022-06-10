c4f6b2467e22c2b848e1df0a34fb99a7cadd0c0f
Deni Maulana Shobri <mobileexpo232@gmail.com>
2022-06-10 13:31:36 +0700

    menambahkan rest response

--- src/main/java/com/indocyber/Elearning/controllers/MateriContoller.java
+++ src/main/java/com/indocyber/Elearning/controllers/MateriContoller.java
@@ -3,9 +3,11 @@ package com.indocyber.Elearning.controllers;
 import com.indocyber.Elearning.dtos.Materi.MateriHeaderDto;
 import com.indocyber.Elearning.dtos.Materi.MateriInsertDto;
 import com.indocyber.Elearning.dtos.Materi.MateriUpdateDto;
+import com.indocyber.Elearning.dtos.RestResponse;
 import com.indocyber.Elearning.models.Materi;
 import com.indocyber.Elearning.services.MateriService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,22 +23,38 @@ public class MateriContoller {
     }
 
     @GetMapping("index")
-    public List<MateriHeaderDto> findAllMateri(){
-        return service.findAllMateri();
+    public ResponseEntity<RestResponse<List<MateriHeaderDto>>> findAllMateri() {
+        return ResponseEntity.ok().body(
+            new RestResponse<List<MateriHeaderDto>>(service.findAllMateri(),
+                    "Success Find All Materi",
+                    200
+            ));
     }
 
     @PostMapping("insert")
-    public MateriInsertDto insertNewMateri(@RequestBody MateriInsertDto insertDto){
-        return service.insertNewMateri(insertDto);
+    public ResponseEntity<RestResponse<MateriInsertDto>>  insertNewMateri(@RequestBody MateriInsertDto insertDto){
+        return ResponseEntity.ok().body(
+            new RestResponse<MateriInsertDto>(service.insertNewMateri(insertDto),
+                    "Success Insert New Materi",
+                    200
+            ));
     }
 
     @PutMapping("{id}")
-    public MateriUpdateDto updateMateri(@PathVariable Integer id, @RequestBody MateriUpdateDto updateDto){
-       return service.UpdateMateri(id,updateDto);
+    public ResponseEntity<RestResponse<MateriUpdateDto>> updateMateri(@PathVariable Integer id, @RequestBody MateriUpdateDto updateDto){
+       return ResponseEntity.ok().body(
+           new RestResponse<MateriUpdateDto>(service.UpdateMateri(id, updateDto),
+                   "Success Update Materi",
+                   200
+           ));
     }
 
     @DeleteMapping("delete/{id}")
-    public List<MateriHeaderDto> deleteMateriById(@PathVariable Integer id){
-       return service.deleteMateriById(id);
+    public ResponseEntity<RestResponse<List<MateriHeaderDto>>> deleteMateriById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(
+            new RestResponse<List<MateriHeaderDto>>(service.deleteMateriById(id),
+                    "Success Delete Materi",
+                    200
+            ));
     }
 }
